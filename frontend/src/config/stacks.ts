8c402dea7270d3b8b9094918689e73f2951bfadc
shaaibu7 <shaaibusuleiman9@gmail.com>
2025-12-26 08:01:19 +0100

    chore: fix stacks network imports

--- frontend/src/config/stacks.ts
+++ frontend/src/config/stacks.ts
@@ -1,10 +1,10 @@
 
 // Basic Stacks network configuration for use with @stacks/connect
-import { StacksTestnet, StacksMainnet } from '@stacks/network';
+import { STACKS_TESTNET, STACKS_MAINNET } from '@stacks/network';
 
 export const stacksNetworks = {
-    testnet: new StacksTestnet(),
-    mainnet: new StacksMainnet(),
+    testnet: STACKS_TESTNET,
+    mainnet: STACKS_MAINNET,
 };
 
 export const activeStacksNetwork = stacksNetworks.testnet;
