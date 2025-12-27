1b12d926474999932eaf80902bb205d7f6cedc47
shaaibu7 <shaaibusuleiman9@gmail.com>
2025-12-27 08:18:05 +0100

    feat: create complete landing page with all sections and update Home component

---
+++ frontend/src/components/landing/index.ts
@@ -0,0 +1,7 @@
+export { default as LandingPage } from './LandingPage'
+export { default as HeroSection } from './HeroSection'
+export { default as FeaturesSection } from './FeaturesSection'
+export { default as StatsSection } from './StatsSection'
+export { default as HowItWorksSection } from './HowItWorksSection'
+export { default as TestimonialsSection } from './TestimonialsSection'
+export { default as FAQSection } from './FAQSection'
